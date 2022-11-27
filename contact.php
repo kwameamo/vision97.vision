4ad03346bc67fca2cb1810727b91147dd317ece8
kwameamo <118262204+kwameamo@users.noreply.github.com>
2022-11-27 00:25:29 +0000

    contact

--- contact.php
+++ contact.php
@@ -1,3 +1,69 @@
+<?php
+
+/**
+ * This example shows how to handle a simple contact form safely.
+ */
+
+//Import PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+
+$msg = '';
+//Don't run this unless we're handling a form submission
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+
+    require '../vendor/autoload.php';
+
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer();
+    //Send using SMTP to localhost (faster and safer than using mail()) – requires a local mail server
+    //See other examples for how to use a remote server such as gmail
+    $mail->isSMTP();
+    $mail->Host = 'localhost';
+    $mail->Port = 25;
+
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('from@example.com', 'First Last');
+    //Choose who the message should be sent to
+    //You don't have to use a <select> like in this example, you can simply use a fixed address
+    //the important thing is *not* to trust an email address submitted from the form directly,
+    //as an attacker can substitute their own and try to use your form to send spam
+    $addresses = ['vision97.vision@gmail.com'];
+    //Validate address selection before trying to use it
+    if (array_key_exists('dept', $_POST) && array_key_exists($_POST['dept'], $addresses)) {
+        $mail->addAddress($addresses[$_POST['dept']]);
+    } else {
+        //Fall back to a fixed address if dept selection is invalid or missing
+        $mail->addAddress('vision97.vision@gmail.com');
+    }
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        //Keep it simple - don't use HTML
+        $mail->isHTML(false);
+        //Build a simple message body
+        $mail->Body = <<<EOT
+        Email: {$_POST['email']}
+        Name: {$_POST['name']}
+        Message: {$_POST['message']}
+        EOT;
+        //Send the message, check for errors
+        if (!$mail->send()) {
+            //The reason for failing to send will be in $mail->ErrorInfo
+            //but it's unsafe to display errors directly to users - process the error, log it on your server.
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+            $msg = 'Message sent! Thanks for contacting us.';
+        }
+    } else {
+        $msg = 'Invalid email address, message ignored.';
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,108 +76,50 @@
     <title>Vision'97 — Contact</title>
 </head>
 <body>
-    <div class="head">
-        <div class="marq">
-                <div class="click-here">
-                    <a href="https://wa.me/message/5P5KBD3UDAXLP1" target="_blank"><span class="click">🇬🇭 Shopping from Ghana? Click Here! 🇬🇭</span></a>  
-                </div>
-        </div>    
-        
 <!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR -->
         <div class="nav">
-            <div class="leftNav">
-                <a href="#1" class="item">Shop</a>
-                <a href="looks.html" class="item">Looks</a>
-                <a href="list.html" class="item">List</a>
-                <a href="news.html" class="item">News</a>
-            </div>
             <div class="logoCentre">
                 <a href="index.html"><img src="images/Vision97LogoW.svg" alt="Vision'97 Logo"></a>
             </div>
-            <div class="rightNav">
+            <div class="bottomNav">
+                <a href="shop.html" class="item">Shop</a>
+                <a href="looks.html" class="item">Looks</a>
                 <a href="terms.html" class="item">Terms</a>
                 <a href="about.html" class="item">About</a>
-                <a href="contact.html" class="item">Contact</a>
-                <a href="#8" class="item">Cart (97)</a>
+                <!-- <a href="#" class="item">Contact</a> -->
+                <a href="#" class="item">Contact</a>
             </div>
         </div>    
-        
     </div>
-
+<!-- NAVBAR --><!-- NAVBAR --><!-- NAVBAR -->
 
     <!-- Content -->
     <div class="about">
             <p> <h2>CONTACT</h2>
             <br>
+
+            <?php if (!empty($msg)) {
+                echo "<h2>$msg</h2>";
+            } ?>
             
             <div class="contact-form">
-
-                <?php
-                    if($_POST) {
-                        $name = "";
-                        $email = "";
-                        $message = "";
-                        $email_body = "<div>";
-                            
-                        if(isset($_POST['name'])) {
-                            $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-                            $email_body .= "<div>
-                                                <label><b>Name:</b></label>&nbsp;<span>".$name."</span>
-                                            </div>";
-                        }
-                    
-                        if(isset($_POST['email'])) {
-                            $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-                            $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-                            $email_body .= "<div>
-                                                <label><b>Email:</b></label>&nbsp;<span>".$email."</span>
-                                            </div>";
-                        }
-                            
-                        if(isset($_POST['message'])) {
-                            $message = htmlspecialchars($_POST['message']);
-                            $email_body .= "<div>
-                                                <label><b>Message:</b></label>
-                                                <div>".$message."</div>
-                                            </div>";
-                        }
-                        {
-                                $recipient = "kwameagain@icloud.com";
-                            }
-
-                        $email_body .= "</div>";
-                    
-                        $headers  = 'MIME-Version: 1.0' . "\r\n"
-                        .'Content-type: text/html; charset=utf-8' . "\r\n"
-                        .'From: ' . $email . "\r\n";
-                            
-                        if(mail($recipient, $email_title, $email_body, $headers)) {
-                            echo 
-                            "
-                            <p>Thank you for contacting us, $name. <br> You will get a reply within 24 hours.</p>
-                            ";
-                        } else {
-                            echo '<p>We are sorry but the email did not go through.</p>';
-                        }
-                            
-                    } else {
-                        echo '<p>Something went wrong</p>';
-                    }
-                ?>
+                <form action="" method="POST">
+                    <input type="text" name="name" placeholder="Name" id="name" required>
+                    <input type="email" name="email" placeholder="Email" id="email" required>
+                    <textarea name="message" id="message" cols="30" rows="5" placeholder="Message" required></textarea>
+                    <input type="submit" name="submit" value="Send">
+                </form>
             </div>
+
         </div>
-
-
 
     <footer>
         <div class="social">
             <h3> <span><a href="https://www.instagram.com/vision97.vision" class="socialLink" target="_blank">INSTAGRAM</a></span> — <span><a href="https://www.twitter.com/vision97.vision" class="socialLink" target="_blank">TWITTER</a></span> — <span><a href="#" class="socialLink" target="_blank">NEWSLETTER</a></span></h3>
-          </div>
-          <div class="copy">
+        </div>
+        <div class="copy">
               <h3>Vision'97® 2022 *the future's clear**</h3>
-          </div>
-    </footer>        
-    </div>
-    
+        </div>
+    </footer>            
 </body>
 </html>
